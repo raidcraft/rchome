de65db67adc180da66013180b2148c1f2cbea475
Philip Urban <urban.philip@googlemail.com>
2013-04-06 18:34:27 +0200

    added permission check

--- src/main/java/de/raidcraft/rchome/listener/PlayerListener.java
+++ src/main/java/de/raidcraft/rchome/listener/PlayerListener.java
@@ -18,11 +18,16 @@ public class PlayerListener implements Listener {
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
 
+        Player player = event.getPlayer();
+
+        if(!player.hasPermission("rchome.bed.sethome")) {
+            return;
+        }
+
         if(event.getClickedBlock() == null || event.getClickedBlock().getType() != Material.BED_BLOCK) {
             return;
         }
 
-        Player player = event.getPlayer();
         if(WorldGuardUtil.INST.isMember(player)) {
             player.setBedSpawnLocation(player.getLocation(), true);
             RaidCraft.getTable(PlayerHomesTable.class).setHome(player);
